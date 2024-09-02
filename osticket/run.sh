3ad920207c8c3b843e9b6f38a97a44fc93cc379b
Gajanan PS <gajananps@yahoo.com>
2024-09-02 12:36:37 +0530

    Update run.sh

--- osticket/run.sh
+++ osticket/run.sh
@@ -3,4 +3,4 @@ currentpath=$(pwd)
 sudo docker run -d --name osTicketDB -e MYSQL_USER=ostuser -e MYSQL_PASSWORD=Admin@123 -e MYSQL_ROOT_PASSWORD=Root@12345 -e MYSQL_DATABASE=osticketdb -v /opt/docker/db:/var/lib/mysql -p 3306:3306 mariadb:10.6.18
 sudo cd $currentpath/app
 sudo docker build -t osticketapp:1.18.1 ./app
-sudo docker run --name osTicketApp -v /opt/docker/app:/var/www/html -p 80:80 -d osticketapp:1.18.1
+sudo docker run --name osTicketApp -p 80:80 -d osticketapp:1.18.1
