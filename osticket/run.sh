bfd430bc9c4e6b7afd5a46ee3b649106a9685213
Gajanan PS <gajananps@yahoo.com>
2024-09-02 10:55:31 +0530

    osTicket Docker Containerization

---
+++ osticket/run.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+currentpath=$(pwd)
+docker run -d --name osTicketDB -e MYSQL_USER=ostuser -e MYSQL_PASSWORD=Admin@123 -e MYSQL_ROOT_PASSWORD=Root@12345 -e MYSQL_DATABASE=osticketdb -v /opt/docker/db:/var/lib/mysql -p 3306:3306 mariadb:10.6.18
+cd $currentpath/app
+docker build -t osticketapp:1.18.1 .
+docker run --name osTicketApp -v /opt/docker/app:/var/www/html -p 80:80 -d osticketapp:1.18.1
